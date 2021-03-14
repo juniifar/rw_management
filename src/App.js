7bb35d3126e29d10fc698275408f72d73e1707e3
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-03-14 18:01:59 +0700

    Adding bootstrap

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactive
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href>Navbar</a>
+      </div>
+      </nav>
+      <h1>Hello Apa Kabar</h1>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn Reactive
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
