cd5a92b591d25329cd4d400dcae1ee7522b1c008
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-03-14 22:23:06 +0700

    Change Color Menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -2,9 +2,9 @@
 const Menu = () => {
     return(
         <nav className="navbar navbar-dark bg-primary">
-        <div className="container-fluid">
-            <a className="navbar-brand" href>RW Management</a>
-        </div>
+            <div className="container-fluid">
+                <a className="navbar-brand" href>RW Management</a>
+            </div>
         </nav>
     );
 };
