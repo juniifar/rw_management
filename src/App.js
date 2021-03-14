949bfe342a145cea06b381434f78a474cb45b509
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-03-14 21:38:50 +0700

    Implement home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -9,7 +9,47 @@ function App() {
         <a className="navbar-brand" href>Navbar</a>
       </div>
       </nav>
-      <h1>Hello Apa Kabar Dunia</h1>
+      <div className="container">
+        <div className="py-3">
+          <div className="card text-center">
+            <div className="card-header">
+              Featured
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            <div class="card-footer text-muted">
+              2 days ago
+            </div>
+          </div>
+        </div>
+        <div className="row gx-3 gy-3 py-3">
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                This is some text within a card body.
+              </div>
+            </div>    
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                This is some text within a card body.
+              </div>
+            </div>    
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                This is some text within a card body.
+              </div>
+            </div>    
+          </div>
+        </div>
+        
+        
+      </div>
     </div>
     // <div className="App">
     //   <header className="App-header">
